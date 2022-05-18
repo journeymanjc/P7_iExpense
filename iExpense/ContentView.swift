f75a09cf01933e01a86baed3563f9b9d34858a11
Jae Cho <jjaecho@gmail.com>
2022-05-18 06:29:58 -0400

    Using UderDefaults to store and retrieve Tap Key.  but there is a better way using property wrapper called @AppStorage

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -10,33 +10,18 @@ import SwiftUI
 
 
 struct ContentView: View {
-	@State private var numbers = [Int]()
-	@State private var currentNumber = 1
+	//@State private var tapCount = 0
+	@State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
 	
     var body: some View {
-		 NavigationView{
-			 VStack{
-				 List {
-					 ForEach(numbers, id:\.self){
-						 Text("Row \($0)")
-					 }
-					 .onDelete(perform: removeRows)
-				 }
-				 Button("Add Number"){
-					 numbers.append(currentNumber)
-					 currentNumber += 1
-				 }
-			 }
-			 .toolbar{
-				 EditButton()
-			 }
+		 Button("Tap count :\(tapCount)"){
+			 tapCount += 1
+			 UserDefaults.standard.set(self.tapCount, forKey: "Tap")
 		 }
 		 
     }
 	
-	func removeRows(at offset: IndexSet) {
-		numbers.remove(atOffsets: offset)
-	}
+
 }
 
 struct ContentView_Previews: PreviewProvider {
