88a2f86788d42a2ec52b607133b7e8f4ad010b51
Jae Cho <jjaecho@gmail.com>
2022-05-18 06:33:26 -0400

    Day 36 Using @AppStorage instead of UserDefaults.  This is a better and clearer way.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -11,12 +11,11 @@ import SwiftUI
 
 struct ContentView: View {
 	//@State private var tapCount = 0
-	@State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-	
+	//@State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+	@AppStorage("tapCount") private var tapCount = 0
     var body: some View {
 		 Button("Tap count :\(tapCount)"){
 			 tapCount += 1
-			 UserDefaults.standard.set(self.tapCount, forKey: "Tap")
 		 }
 		 
     }
