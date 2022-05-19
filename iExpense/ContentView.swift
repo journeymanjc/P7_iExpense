872c987c96a489d1966613a8faaaa7a4557164ba
Jae Cho <jjaecho@gmail.com>
2022-05-19 17:36:18 -0400

    Day 38 Challenge. Use the user’s preferred currency, rather than always using US dollars. Modify the expense amounts in ContentView to contain some styling depending on their value – expenses under $10 should have one style, expenses under $100 another, and expenses over $100 a third style. What those styles are depend on you. For a bigger challenge, try splitting the expenses list into two sections: one for personal expenses, and one for business expenses. This is tricky for a few reasons, not least because it means being careful about how items are deleted!

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -33,14 +33,27 @@ class Expenses: ObservableObject{
 	}
 }
 
+//Need to split the expenses into two sections one for personal and another for business.
+/**
+ Approach :
+ 1. figure out how i'm going to separate the data and then worry about presentation.
+ 1.1 separating the data first by filtering them between personal and business
+ 1.2 Create a separate list for personal and business in the listview.
+ 1.3 will need to incex them separately.
+ */
+
 struct ContentView: View {
 	@StateObject var expenses = Expenses()
 	@State private var showingAddExpense = false
-	
+
     var body: some View {
+		 
 		 NavigationView{
+			 VStack{
+				 
+			 }
 			 List{
-				 ForEach(expenses.items){ item in
+				 ForEach(categorizeList(with: "Business")){ item in
 					 HStack{
 						 VStack(alignment: .leading){
 							 Text(item.name)
@@ -48,7 +61,8 @@ struct ContentView: View {
 							 Text(item.type)
 						 }
 						 Spacer()
-						 Text(item.amount, format: .currency(code: "USD"))
+						 MyTextView(amount: item.amount)
+						 
 					 }
 				 }
 				 .onDelete(perform: removeItems)
@@ -60,6 +74,7 @@ struct ContentView: View {
 					 Image(systemName: "plus")
 				 }
 			 }
+			 
 		 }
 		 .sheet(isPresented: $showingAddExpense) {
 			 //show an AddView here
@@ -70,8 +85,40 @@ struct ContentView: View {
 	func removeItems(at offsets: IndexSet) {
 		expenses.items.remove(atOffsets: offsets)
 	}
+	
+	func categorizeList(with category: String) -> [ExpenseItem]{
+		let sublist = expenses.items.filter{ $0.type == category }
+		return sublist
+	}
 
 }
+
+
+
+
+struct MyTextView: View{
+	@State  var amount : Double
+	
+	var body: some View{
+		switch amount{
+		case let amount where amount < 10.0 :
+			Text(amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+				.font(.system(size: 10))
+				.foregroundColor(.blue)
+		case let amount where amount < 100.0 :
+			Text(amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+				.font(.system(size: 15))
+				.foregroundColor(.orange)
+		case let amount where amount >= 100.0 :
+			Text(amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+				.font(.system(size: 17))
+				.foregroundColor(.red)
+		default :
+			Text(amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+		}
+	}
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
